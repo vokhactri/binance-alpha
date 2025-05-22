074e35cddf7e3640ab3d2e1d28a68128aaec035a
holazz <2418184580@qq.com>
2025-05-22 12:31:44 +0800

    perf: stored timestamp

--- hooks/use-blocks.ts
+++ hooks/use-blocks.ts
@@ -1,13 +1,31 @@
 import axios from 'axios'
-import { useQuery, } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
+import { useLocalStorage } from '@/hooks/use-local-storage'
+
+interface BlockCache {
+  timestamp: number
+  blockNumber: number
+}
 
 export const useBlockNumber = (timestamp: number) => {
+  const [blockCache, setBlockCache] = useLocalStorage<BlockCache | null>('blockCache', null)
   return useQuery({
     queryKey: ['blocks', timestamp],
-    queryFn: () =>
-      axios.get('/api/blocks', {
+    queryFn: async () => {
+      if (blockCache?.timestamp === timestamp) {
+        return {
+          data: blockCache.blockNumber,
+        }
+      }
+      const response = await axios.get('/api/blocks', {
         params: { timestamp },
-      }),
+      })
+      setBlockCache({
+        timestamp,
+        blockNumber: response.data,
+      })
+      return response
+    },
     enabled: !isNaN(timestamp),
     select: (response) => response.data,
   })
