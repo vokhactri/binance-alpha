3e3e1eeb0353bcd61bdf86f9b7dd5a5089b190aa
holazz <2418184580@qq.com>
2025-05-30 11:01:06 +0800

    feat: add alert

---
+++ components/layout/alert.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from 'react'
+import { X, InfoIcon } from 'lucide-react'
+import { Alert as _Alert, AlertDescription } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+
+export default function Alert({ children }: { children: React.ReactNode }) {
+  const [showAlert, setShowAlert] = useState(true)
+
+  return (
+    <>
+      {showAlert && (
+        <_Alert className="w-full max-w-5xl bg-yellow-50 dark:bg-yellow-950 border-yellow-300 dark:border-yellow-800">
+          <InfoIcon size={10} className="!text-yellow-500" />
+          <AlertDescription className="text-yellow-700 dark:text-yellow-300 flex justify-between items-center">
+            {children}
+            <Button
+              variant="ghost"
+              size="sm"
+              onClick={() => setShowAlert(false)}
+              className="h-auto hover:bg-transparent"
+            >
+              <X className="h-4 w-4" />
+            </Button>
+          </AlertDescription>
+        </_Alert>
+      )}
+    </>
+  )
+}
