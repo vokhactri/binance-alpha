96e0480bee0b68323d42feb1704709f9b63d98f1
holazz <holazz@users.noreply.github.com>
2025-07-12 07:26:36 +0000

    chore: update alpha token list

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -3,6 +3,13 @@ import type { AlphaTokenInfo } from '@/types'
 const tokens: AlphaTokenInfo[] = [
   {
     chainId: '56',
+    contractAddress: '0x8b9ee39195ea99d6ddd68030f44131116bc218f6',
+    name: `peaq`,
+    symbol: 'PEAQ',
+    decimals: 18,
+  },
+  {
+    chainId: '56',
     contractAddress: '0x37eff3ff1321fb9abc734761ca72fafdc044534a',
     name: `PrompTale AI`,
     symbol: 'TALE',
