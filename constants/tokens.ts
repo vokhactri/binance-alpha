f2dbb480f8c703e0990d4f38ad9daaa010916f65
holazz <holazz@users.noreply.github.com>
2025-07-15 10:33:49 +0000

    chore: update alpha token list

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -3,6 +3,13 @@ import type { AlphaTokenInfo } from '@/types'
 const tokens: AlphaTokenInfo[] = [
   {
     chainId: '56',
+    contractAddress: '0x1219c409fabe2c27bd0d1a565daeed9bd9f271de',
+    name: `TAC`,
+    symbol: 'TAC',
+    decimals: 18,
+  },
+  {
+    chainId: '56',
     contractAddress: '0xc32cc70741c3a8433dcbcb5ade071c299b55ffc8',
     name: `Chainbase`,
     symbol: 'C',
