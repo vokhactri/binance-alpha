c17bc97e6f68d6e3484f003a781faf1cff085398
vokhactri <vokhactri@users.noreply.github.com>
2025-09-24 11:21:48 +0000

    chore: update alpha token list

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -3,6 +3,13 @@ import type { AlphaTokenInfo } from '@/types'
 const tokens: AlphaTokenInfo[] = [
   {
     chainId: '56',
+    contractAddress: '0xacf5a368ec5bb9e804c8ac0b508daa5a21c92e13',
+    name: `Griffin AI`,
+    symbol: 'GAIN',
+    decimals: 18,
+  },
+  {
+    chainId: '56',
     contractAddress: '0x7c8217517ed4711fe2deccdfeffe8d906b9ae11f',
     name: `Bless`,
     symbol: 'BLESS',
