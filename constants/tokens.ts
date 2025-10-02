aa4e6368d013aec0a67c23f48127c96373486013
vokhactri <vokhactri@users.noreply.github.com>
2025-10-02 09:41:51 +0000

    chore: update alpha token list

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -3,6 +3,13 @@ import type { AlphaTokenInfo } from '@/types'
 const tokens: AlphaTokenInfo[] = [
   {
     chainId: '56',
+    contractAddress: '0x0a43fc31a73013089df59194872ecae4cae14444',
+    name: `4`,
+    symbol: '4',
+    decimals: 18,
+  },
+  {
+    chainId: '56',
     contractAddress: '0x2aa89a0113bcbbcdc5812c6df794e2d9650fc1af',
     name: `StrikeBit AI`,
     symbol: 'STRIKE',
