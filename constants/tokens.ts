cae9c9ffe52524c59f0d0e41beb2030eab0fcdc1
vokhactri <vokhactri@users.noreply.github.com>
2025-11-29 08:32:42 +0000

    chore: update alpha token list

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -3,6 +3,13 @@ import type { AlphaTokenInfo } from '@/types'
 const tokens: AlphaTokenInfo[] = [
   {
     chainId: '56',
+    contractAddress: '0xc12efb9e4a1a753e7f6523482c569793c2271dbb',
+    name: `GaiAI Token`,
+    symbol: 'GAIX',
+    decimals: 18,
+  },
+  {
+    chainId: '56',
     contractAddress: '0xa5c8e1513b6a08334b479fe4d71f1253259469be',
     name: `SUPERFORTUNE`,
     symbol: 'GUA',
